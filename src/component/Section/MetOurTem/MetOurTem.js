2c7cbf1eb15068d8fe7f4b4a05b4e825fe11ca3c
Kakon Ray <thisiskakonray@gmail.com>
2022-04-29 15:47:31 +0600

    slick slider completed

--- src/component/Section/MetOurTem/MetOurTem.js
+++ src/component/Section/MetOurTem/MetOurTem.js
@@ -1,11 +1,163 @@
-import React from 'react';
+import React from "react";
 
+import Slider from "react-slick";
+import img1 from "../../../assets/img/team-1.jpg";
+import img2 from "../../../assets/img/team-2.jpg";
+import img3 from "../../../assets/img/team-3.jpg";
+import "./MetOurTem.css";
 const MetOurTem = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="container mx-auto px-20 my-30">
+      <div className="my-12 text-center">
+        <h1 className="text-2xl mb-2">MEET OUR TEAM</h1>
+        <p>At vero eos et accusam et justo duo dolores et ea rebum.</p>
+      </div>
+      <div className="w-100">
+        <Slider {...settings}>
+          <div>
+            <div className="grid grid-cols-2 gap-10">
+              <div class="flex justify-center">
+                <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                  <img
+                    class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                    src={img1}
+                    alt=""
+                  />
+                  <div class="p-6 flex flex-col justify-start">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">
+                      Card title
+                    </h5>
+                    <p class="text-gray-700 text-base mb-4">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                  </div>
+                </div>
+              </div>
+              <div class="flex justify-center">
+                <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                  <img
+                    class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                    src={img2}
+                    alt=""
+                  />
+                  <div class="p-6 flex flex-col justify-start">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">
+                      Card title
+                    </h5>
+                    <p class="text-gray-700 text-base mb-4">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="grid grid-cols-2 gap-10">
+              <div class="flex justify-center">
+                <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                  <img
+                    class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                    src={img1}
+                    alt=""
+                  />
+                  <div class="p-6 flex flex-col justify-start">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">
+                      Card title
+                    </h5>
+                    <p class="text-gray-700 text-base mb-4">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                  </div>
+                </div>
+              </div>
+              <div class="flex justify-center">
+                <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                  <img
+                    class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                    src={img2}
+                    alt=""
+                  />
+                  <div class="p-6 flex flex-col justify-start">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">
+                      Card title
+                    </h5>
+                    <p class="text-gray-700 text-base mb-4">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="grid grid-cols-2 gap-10">
+              <div class="flex justify-center">
+                <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                  <img
+                    class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                    src={img1}
+                    alt=""
+                  />
+                  <div class="p-6 flex flex-col justify-start">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">
+                      Card title
+                    </h5>
+                    <p class="text-gray-700 text-base mb-4">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                  </div>
+                </div>
+              </div>
+              <div class="flex justify-center">
+                <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                  <img
+                    class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                    src={img2}
+                    alt=""
+                  />
+                  <div class="p-6 flex flex-col justify-start">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">
+                      Card title
+                    </h5>
+                    <p class="text-gray-700 text-base mb-4">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
 };
 
 export default MetOurTem;
